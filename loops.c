6ff32eff27a28d926268b889eaaa0e22ae02dafe
binbashz <5732@holbertonstudnets.com>
2023-02-15 15:38:07 -0800

    commit

---
+++ loops.c
@@ -0,0 +1,27 @@
+
+
+/* El bucle for :
+ *  Sintaxis
+ *	for(inicializacion; condicion; increment)
+ *	Instrucciones; */
+
+
+/* mostrar los 16 primeros numeros - ascendente */
+
+#include <stdio.h>
+
+	int main(void)
+{
+	int i;
+	for(i=1; i<=16; i++)
+	{
+		printf("%i.\n",i);
+	}
+
+
+
+
+
+
+	return (0);
+}
